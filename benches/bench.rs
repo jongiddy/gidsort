816e598704692bac03af6305d355404399fe6784
Jonathan Giddy <jongiddy@gmail.com>
2017-02-20 22:33:28 +0000

    Add benchmark derived from stjepang/faster-sort

---
+++ benches/bench.rs
@@ -0,0 +1,123 @@
+#![feature(test)]
+
+extern crate rand;
+extern crate test;
+
+extern crate gidsort;
+
+use rand::{thread_rng, Rng};
+use std::mem;
+use test::Bencher;
+
+
+fn gen_ascending(len: usize) -> Vec<u64> {
+    (0..len as u64).collect()
+}
+
+fn gen_descending(len: usize) -> Vec<u64> {
+    (0..len as u64).rev().collect()
+}
+
+fn gen_random(len: usize) -> Vec<u64> {
+    let mut rng = thread_rng();
+    rng.gen_iter::<u64>().take(len).collect()
+}
+
+fn gen_mostly_ascending(len: usize) -> Vec<u64> {
+    let mut rng = thread_rng();
+    let mut v = gen_ascending(len);
+    for _ in (0usize..).take_while(|x| x * x <= len) {
+        let x = rng.gen::<usize>() % len;
+        let y = rng.gen::<usize>() % len;
+        v.swap(x, y);
+    }
+    v
+}
+
+fn gen_mostly_descending(len: usize) -> Vec<u64> {
+    let mut rng = thread_rng();
+    let mut v = gen_descending(len);
+    for _ in (0usize..).take_while(|x| x * x <= len) {
+        let x = rng.gen::<usize>() % len;
+        let y = rng.gen::<usize>() % len;
+        v.swap(x, y);
+    }
+    v
+}
+
+fn gen_big_random(len: usize) -> Vec<[u64; 16]> {
+    let mut rng = thread_rng();
+    rng.gen_iter().map(|x| [x; 16]).take(len).collect()
+}
+
+fn gen_big_ascending(len: usize) -> Vec<[u64; 16]> {
+    (0..len as u64).map(|x| [x; 16]).take(len).collect()
+}
+
+fn gen_big_descending(len: usize) -> Vec<[u64; 16]> {
+    (0..len as u64).rev().map(|x| [x; 16]).take(len).collect()
+}
+
+macro_rules! sort_bench {
+    ($name:ident, $gen:expr, $len:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            b.iter(|| $gen($len).sort());
+            b.bytes = $len * mem::size_of_val(&$gen(1)[0]) as u64;
+        }
+    }
+}
+
+sort_bench!(small_random_306035c21, gen_random, 10);
+sort_bench!(small_ascending_306035c21, gen_ascending, 10);
+sort_bench!(small_descending_306035c21, gen_descending, 10);
+
+sort_bench!(small_big_random_306035c21, gen_big_random, 10);
+sort_bench!(small_big_ascending_306035c21, gen_big_ascending, 10);
+sort_bench!(small_big_descending_306035c21, gen_big_descending, 10);
+
+sort_bench!(medium_random_306035c21, gen_random, 100);
+sort_bench!(medium_ascending_306035c21, gen_ascending, 100);
+sort_bench!(medium_descending_306035c21, gen_descending, 100);
+
+sort_bench!(large_random_306035c21, gen_random, 10000);
+sort_bench!(large_ascending_306035c21, gen_ascending, 10000);
+sort_bench!(large_descending_306035c21, gen_descending, 10000);
+sort_bench!(large_mostly_ascending_306035c21, gen_mostly_ascending, 10000);
+sort_bench!(large_mostly_descending_306035c21, gen_mostly_descending, 10000);
+
+sort_bench!(large_big_random_306035c21, gen_big_random, 10000);
+sort_bench!(large_big_ascending_306035c21, gen_big_ascending, 10000);
+sort_bench!(large_big_descending_306035c21, gen_big_descending, 10000);
+
+macro_rules! new_sort_bench {
+    ($name:ident, $gen:expr, $len:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            b.iter(|| gidsort::sort(&mut $gen($len)));
+            b.bytes = $len * mem::size_of_val(&$gen(1)[0]) as u64;
+        }
+    }
+}
+
+new_sort_bench!(small_random_gidsort, gen_random, 10);
+new_sort_bench!(small_ascending_gidsort, gen_ascending, 10);
+new_sort_bench!(small_descending_gidsort, gen_descending, 10);
+
+new_sort_bench!(small_big_random_gidsort, gen_big_random, 10);
+new_sort_bench!(small_big_ascending_gidsort, gen_big_ascending, 10);
+new_sort_bench!(small_big_descending_gidsort, gen_big_descending, 10);
+
+new_sort_bench!(medium_random_gidsort, gen_random, 100);
+new_sort_bench!(medium_ascending_gidsort, gen_ascending, 100);
+new_sort_bench!(medium_descending_gidsort, gen_descending, 100);
+
+new_sort_bench!(large_random_gidsort, gen_random, 10000);
+new_sort_bench!(large_ascending_gidsort, gen_ascending, 10000);
+new_sort_bench!(large_descending_gidsort, gen_descending, 10000);
+new_sort_bench!(large_mostly_ascending_gidsort, gen_mostly_ascending, 10000);
+new_sort_bench!(large_mostly_descending_gidsort, gen_mostly_descending, 10000);
+
+new_sort_bench!(large_big_random_gidsort, gen_big_random, 10000);
+new_sort_bench!(large_big_ascending_gidsort, gen_big_ascending, 10000);
+new_sort_bench!(large_big_descending_gidsort, gen_big_descending, 10000);
