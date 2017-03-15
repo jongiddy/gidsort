e161565f156ce6157a733f7dfb31c4d18614a462
Jonathan Giddy <jongiddy@gmail.com>
2017-03-15 07:07:20 +0000

    Refactor to use parameters to determine start and growth of galloping

--- src/lib.rs
+++ src/lib.rs
@@ -16,40 +16,9 @@ extern crate quickcheck;
 macro_rules! rotate_reverse_max {() => (8)}
 
 
-#[inline]
-fn split_biased(n: usize) -> usize {
-    // Return a split point that ensures that the RHS is a balanced binary tree.  A balanced binary
-    // tree contains 2^k - 1 nodes. Hence the length can be represented in binary by all-ones on
-    // the right (e.g. 15 = 00001111).  Masking off the leftmost 1 of this value gives 7 (00000111)
-    // a.k.a. 2^(k-1) - 1.
-    //
-    // Testing the value at position 7 tells us whether the next test should be on the 7 values
-    // below 7 (0-6) or the 7 values above 7 (8-14). This is the ideal binary search requiring
-    // log2 n tests for any search.
-    //
-    // Initially, we may get a sequence which has between 2^(k-1) and 2^k - 2 nodes, i.e. an
-    // unbalanced binary tree.  In this case, some of the branches may require fewer tests than
-    // others.  Since we are comparing the starts of sorted sequences, we would like to bias the
-    // selection of shorter branches to use fewer tests on the left side.
-    //
-    // This is done by ensuring the right side of the test still returns 2^(k-1) - 1 nodes and the
-    // left side gets the smaller sequence to test.  Conveniently, this is also achieved by masking
-    // off the highest 1 bit.  This technique gives us somewhat of an equivalent to galloping in
-    // other sorting algorithms.
-    //
-    // For example, a sequence of 21 elements has length 00010101.  Masking off the high 1 bit
-    // gives 00000101 = 5. If we test the value at position 5, the next test will either be 6-20
-    // (15 values, a balanced binary tree requiring 4 more tests) or 0-4 (5 values). Removing the
-    // leftmost bit of 5 (00000101) gives 1.  Testing 1 wi1l make the next test 2-4 (3 values, a
-    // balanced binary tree requiring 2 more tests) or 0, requiring 1 additional test.  Hence, the
-    // rightmost values require 5 tests, the leftmost value requires 3 tests, and other low values
-    // require 4 tests.
-
-    let mask = ((n >> 1) + 1).next_power_of_two();
-    n & !mask
-}
-
-fn insertion_point<T, F>(value: &T, buffer: &[T], compare: &F, when_equal: Ordering) -> usize
+fn insertion_point<T, F>(
+    value: &T, buffer: &[T], compare: &F, when_equal: Ordering, initial: usize, offset: usize
+) -> usize
     where
         F: Fn(&T, &T) -> Ordering
 {
@@ -65,9 +34,31 @@ fn insertion_point<T, F>(value: &T, buffer: &[T], compare: &F, when_equal: Order
     let length = buffer.len();
     let mut lo = 0;       // lowest candidate
     let mut hi = length;  // highest candidate
-    let mut p2 = 1;
-    while p2 <= length {
-        let trial = p2 - 1;
+
+    // First, gallop from the start of the sequence.  Galloping provides a middle ground between
+    // pure binary search, which uses log2 n comparisons to find any position, and linear search,
+    // which uses fewer comparisons for the earlier elements, but more for large runs.  Two of the
+    // most common searches are:
+    // - purely random, in which case there are probably only 1 or 2 elements at the start of a
+    // subsequence that are less than the minimum element from the other sequence.
+    // - mostly ordered, in which case comparing the left element against the zeroth element of the
+    // righthand sequence should be fast.
+    //
+    // The gallop starts off slow at the start of the sequence, but increases exponentially to find
+    // a range containing the position.  For example, with initial = 0 and offset = 1, the searched
+    // positions are: 0, 1, 3, 7, 15, 31,...
+    //
+    // If the position is more likely to be later in the sequence, the other values can be used.
+    // With initial = 1, offset = 2 the code tests positions: 1, 3, 7, 15, ... using one less
+    // comparison for most values, at the cost of one additional comparison for position 0.
+    //
+    // The offset value must be a power of two and the initial value must be one less than a power
+    // of two, to ensure that the next stage gets a balanced binary tree.
+    debug_assert!(initial >= length || (initial + 1).is_power_of_two());
+    debug_assert!(offset.is_power_of_two());
+    let mut p2 = offset;
+    while p2 - offset + initial < length {
+        let trial = p2 - offset + initial;
         let mut leg = compare(value, &buffer[trial]);
         if leg == Ordering::Equal {
             leg = when_equal;
@@ -78,7 +69,7 @@ fn insertion_point<T, F>(value: &T, buffer: &[T], compare: &F, when_equal: Order
                 break;
             },
             Ordering::Greater => {
-                lo = p2;
+                lo = trial + 1;
                 p2 *= 2;
             },
             Ordering::Equal => {
@@ -86,6 +77,11 @@ fn insertion_point<T, F>(value: &T, buffer: &[T], compare: &F, when_equal: Order
             }
         }
     }
+
+    // At this point, either hi == length, and we're processing the rump of the sequence, or
+    // lo-hi is a balanced binary tree containing the correct position.  A balanced binary tree
+    // contains 2^n - 1 elements.  Perform binary search to find the final insertion position.
+    debug_assert!(hi == length || (hi - lo + 1).is_power_of_two());
     while hi > lo {
         let trial = lo + (hi - lo) / 2;
         debug_assert!(trial < length);
@@ -251,28 +247,19 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
 
     // R may contain values that are higher than l_max.  These values are already in their final
     // position, so we can move them from R to S1.
-    let pos = insertion_point(&s[m0 - 1], &s[r0 .. r1], compare, leftright);
+    let pos = insertion_point(&s[m0 - 1], &s[r0 .. r1], compare, leftright, 0, 1);
     if pos == 0 {
         // l_max < r_0 -> L-R is already sorted
         //
         // Although this code is shrinking the size of the sequence and setting up a useful
         // invariant, it also provides a third behaviour which is useful when this function is
-        // called as part of a mergesort, which typically merges blocks of size 2^n.
-        // Since that is one more 2^n - 1, the size of a balanced binary tree, `split_biased` must
-        // use a tree of size 2^(n+1) - 1, and since it is biased to the left, only one comparison
-        // is required to check the value against the first point of the sequence.
-        //
-        // See the test `bisect_2pow` for an example.  In the test, /some/ of the paths must
-        // require 5 comparisons.  We bias the algorithm so that one common path is significantly
-        // advantaged at the cost of /all/ other paths requiring 5 comparisons.
+        // called as part of a mergesort. It uses only one comparison to check the value against
+        // the first point of the sequence.
         //
         // This means already ordered sequences are merged with one comparison, and an entire
         // mergesort of already ordered data will take the minimum possible (n-1) comparisons.
         // This is useful because much real data is close to already sorted, so optimising this
-        // case is valuable.  Many other real-world sorts special-case for already sorted data,
-        // often at the cost of worst-case performance for randomly ordered data.  In our case, the
-        // single comparison is performed without affecting the worst-case performance on random
-        // data.
+        // case is valuable.
         return;
     }
     r1 = r0 + pos;
@@ -281,7 +268,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
     // L may contain values that are lower than r_0.  These values are already in their final
     // position, so we can move them from L to S0.  Note, we ignore l_max since we know it is
     // larger than r_0.  This is why we don't need to test whether |L| = 0.
-    let pos = insertion_point(&s[r0], &s[l0 .. m0 - 1], compare, rightleft);
+    let pos = insertion_point(&s[r0], &s[l0 .. m0 - 1], compare, rightleft, 0, 1);
     l0 += pos;
     // r0 is smallest value
 
@@ -301,7 +288,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
 
     // find X in R where X[i] < L[0]
     // - Since R[0] is minimum, L[0] > R[0], so exclude R[0] from search
-    let mut xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright) + 1;
+    let mut xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright, 0, 1) + 1;
     loop {
         debug_assert!(mlen!() == 0);    // |M| == 0
         if xlen == rlen!() {
@@ -316,7 +303,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
         // - Since R[r0 + xlen] > L[0] from previous search, exclude L[0] from search
         // - this search relies on invariant |L| > 1, tested in assert
         debug_assert!(l0 + 1 <= m0 - 1);
-        let zlen = insertion_point(&s[r0 + xlen], &s[l0 + 1 .. m0 - 1], compare, rightleft) + 1;
+        let zlen = insertion_point(&s[r0 + xlen], &s[l0 + 1 .. m0 - 1], compare, rightleft, 0, 1) + 1;
         if llen!() < xlen + zlen {
             // |L| < 2|X| + |Z|:
             // Method E1
@@ -332,7 +319,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
                 return;
             }
             debug_assert!(r0 + 1 < r1);
-            xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright) + 1;
+            xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright, 0, 1) + 1;
         }
         else {
             // Method E2
@@ -353,7 +340,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
             debug_assert!(mlen!() > 0); // |M| > 0
             // find X in R where X[i] < M[0]
             debug_assert!(r0 + 1 < r1);
-            xlen = insertion_point(&s[m0], &s[r0 + 1 .. r1], compare, leftright) + 1;
+            xlen = insertion_point(&s[m0], &s[r0 + 1 .. r1], compare, leftright, 0, 1) + 1;
             loop {
                 if llen!() < xlen {
                     // |L| < |X|:
@@ -374,11 +361,11 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
                     return
                 }
                 // find Y in M where Y[i] < R'[0]
-                let ylen = insertion_point(&s[r0 + xlen], &s[m0 + 1 .. r0], compare, rightleft) + 1;
+                let ylen = insertion_point(&s[r0 + xlen], &s[m0 + 1 .. r0], compare, rightleft, 0, 1) + 1;
                 if ylen == mlen!() {
                     // |Y| == |M|:
                     // find Z in L where Z[i] < R'[0]
-                    let zlen = insertion_point(&s[r0 + xlen], &s[l0 .. m0 - 1], compare, rightleft);
+                    let zlen = insertion_point(&s[r0 + xlen], &s[l0 .. m0 - 1], compare, rightleft, 0, 1);
                     // Methods C1 and C3 both start with a rotate of Y - X
                     rotate(&mut s[m0 .. r0 + xlen], xlen);
                     if llen!() < xlen + ylen + zlen {
@@ -397,7 +384,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
                         }
                         // find X in R where X[i] < L[0]
                         debug_assert!(r0 + 1 < r1);
-                        xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright) + 1;
+                        xlen = insertion_point(&s[l0], &s[r0 + 1 .. r1], compare, leftright, 0, 1) + 1;
                         break
                     }
                     // Method C3
@@ -442,7 +429,7 @@ fn merge<T, F>(s: &mut [T], split: usize, compare: &F, leftright: Ordering, righ
                     return;
                 }
                 debug_assert!(r0 + 1 < r1);
-                xlen = insertion_point(&s[m0], &s[r0 + 1 .. r1], compare, leftright) + 1;
+                xlen = insertion_point(&s[m0], &s[r0 + 1 .. r1], compare, leftright, 0, 1) + 1;
             }
         }
     }
@@ -737,89 +724,49 @@ mod tests {
     }
 
     #[test]
-    fn split_biased_0() {
-        assert_eq!(super::split_biased(0), 0);
-    }
-
-    #[test]
-    fn split_biased_1() {
-        assert_eq!(super::split_biased(1), 0);
-    }
-
-    #[test]
-    fn split_biased_2() {
-        assert_eq!(super::split_biased(2), 0);
-    }
-
-    #[test]
-    fn split_biased_3() {
-        assert_eq!(super::split_biased(3), 1);
-    }
-
-    #[test]
-    fn split_biased_2powm1() {
-        assert_eq!(super::split_biased(31), 15);
-    }
-
-    #[test]
-    fn split_biased_2pow() {
-        assert_eq!(super::split_biased(32), 0);
-    }
-
-    #[test]
-    fn split_biased_2powp1() {
-        assert_eq!(super::split_biased(33), 1);
-    }
-
-    #[test]
-    fn split_biased_10101() {
-        assert_eq!(super::split_biased(21), 5)
-    }
-
-    #[test]
     fn bisect_0() {
-        assert_eq!(super::insertion_point(&Nc(3), &[], &Nc::cmp, Ordering::Less), 0)
+        assert_eq!(super::insertion_point(&Nc(3), &[], &Nc::cmp, Ordering::Less, 0, 1), 0)
     }
 
     #[test]
     fn bisect_1_before() {
-        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2)], &Nc::cmp, Ordering::Less), 0)
+        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2)], &Nc::cmp, Ordering::Less, 0, 1), 0)
     }
     #[test]
     fn bisect_1_after() {
-        assert_eq!(super::insertion_point(&Nc(3), &[Nc(2)], &Nc::cmp, Ordering::Less), 1)
+        assert_eq!(super::insertion_point(&Nc(3), &[Nc(2)], &Nc::cmp, Ordering::Less, 0, 1), 1)
     }
 
     #[test]
     fn bisect_2_before() {
-        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less), 0)
+        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less, 0, 1), 0)
     }
     #[test]
     fn bisect_2_middle() {
-        assert_eq!(super::insertion_point(&Nc(3), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less), 1)
+        assert_eq!(super::insertion_point(&Nc(3), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less, 0, 1), 1)
     }
     #[test]
     fn bisect_2_after() {
-        assert_eq!(super::insertion_point(&Nc(5), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less), 2)
+        assert_eq!(super::insertion_point(&Nc(5), &[Nc(2), Nc(4)], &Nc::cmp, Ordering::Less, 0, 1), 2)
     }
 
     #[test]
     fn bisect_3_before() {
-        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less), 0)
+        assert_eq!(super::insertion_point(&Nc(1), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less, 0, 1), 0)
     }
     #[test]
     fn bisect_3_lt() {
         // Use less-than if the value should be inserted before equal values
-        assert_eq!(super::insertion_point(&Nc(4), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less), 1)
+        assert_eq!(super::insertion_point(&Nc(4), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less, 0, 1), 1)
     }
     #[test]
     fn bisect_3_le() {
         // Use less-than-or-equal if value should be inserted after equal values
-        assert_eq!(super::insertion_point(&Nc(4), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Greater), 2)
+        assert_eq!(super::insertion_point(&Nc(4), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Greater, 0, 1), 2)
     }
     #[test]
     fn bisect_3_after() {
-        assert_eq!(super::insertion_point(&Nc(7), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less), 3)
+        assert_eq!(super::insertion_point(&Nc(7), &[Nc(2), Nc(4), Nc(6)], &Nc::cmp, Ordering::Less, 0, 1), 3)
     }
 
     #[test]
@@ -828,7 +775,7 @@ mod tests {
         let mut profile = Vec::new();
         for v in 0 .. s.len() + 1 {
             let count = Cell::new(0);
-            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less), v);
+            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less, s.len(), 1), v);
             profile.push(count.get());
         }
         assert_eq!(profile, vec![4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4])
@@ -840,10 +787,10 @@ mod tests {
         let mut profile = Vec::new();
         for v in 0 .. s.len() + 1 {
             let count = Cell::new(0);
-            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less), v);
+            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less, 0, 1), v);
             profile.push(count.get());
         }
-        assert_eq!(profile, vec![1, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+        assert_eq!(profile, vec![1, 2, 4, 4, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 8, 8, 5])
     }
 
     #[test]
@@ -852,44 +799,44 @@ mod tests {
         let mut profile = Vec::new();
         for v in 0 .. s.len() + 1 {
             let count = Cell::new(0);
-            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less), v);
+            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less, 0, 1), v);
             profile.push(count.get());
         }
-        assert_eq!(profile, vec![2, 2, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+        assert_eq!(profile, vec![1, 2, 4, 4, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 8, 8, 6, 6])
     }
 
     #[test]
-    fn bisect_20() {
+    fn bisect_20_0_1() {
         let s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
         let mut profile = Vec::new();
         for v in 0 .. s.len() + 1 {
             let count = Cell::new(0);
-            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less), v);
+            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less, 0, 1), v);
             profile.push(count.get());
         }
-        assert_eq!(profile, vec![2, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+        assert_eq!(profile, vec![1, 2, 4, 4, 6, 6, 6, 6, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7])
     }
 
     #[test]
-    fn bisect_21() {
-        let s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    fn bisect_20_1_2() {
+        let s = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
         let mut profile = Vec::new();
         for v in 0 .. s.len() + 1 {
             let count = Cell::new(0);
-            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less), v);
+            assert_eq!(super::insertion_point(&v, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Less, 1, 2), v);
             profile.push(count.get());
         }
-        assert_eq!(profile, vec![3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5])
+        assert_eq!(profile, vec![2, 2, 3, 3, 5, 5, 5, 5, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6])
     }
 
     #[test]
     fn bisect_unstable() {
         // If Ordering::Equal passed, then insertion point returned on first match
-        // Here we have 2^n -1 values, so first test will be in middle. Since that matches the
-        // value, it will return immediately.
+        // Here we pass the length as the initial value, so first test will be in middle.
+        // Since that matches the value, it will return immediately.
         let s = [1, 5, 5, 5, 5, 5, 8];
         let count = Cell::new(0);
-        super::insertion_point(&5, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Equal);
+        super::insertion_point(&5, &s, &|&a, &b|{count.set(count.get() + 1); a.cmp(&b)}, Ordering::Equal, s.len(), 1);
         assert_eq!(count.get(), 1);
     }
 
