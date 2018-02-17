4829ce38f33a8cacef2c10adda50811e705e4a79
Jonathan Giddy <jongiddy@gmail.com>
2018-02-17 07:45:55 +0000

    Add sort_unstable to benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -59,6 +59,38 @@ fn gen_short_runs(len: usize) -> Vec<u64> {
     v
 }
 
+fn gen_nightmare(_: usize) -> Vec<u64> {
+    let mut left = Vec::<u64>::new();
+    let mut right = Vec::<u64>::new();
+    let mut val = 0;
+    for i in 2 .. 513 {
+        right.push(val);
+        val += 1;
+        for _ in 0 .. i {
+            left.push(val);
+            val += 1;
+        }
+    }
+    left.append(&mut right);
+    left
+}
+
+fn gen_marenight(_: usize) -> Vec<u64> {
+    let mut left = Vec::<u64>::new();
+    let mut right = Vec::<u64>::new();
+    let mut val = 0;
+    for i in 2 .. 513 {
+        right.push(val);
+        val += 1;
+        for _ in 0 .. i {
+            left.push(val);
+            val += 1;
+        }
+    }
+    right.append(&mut left);
+    right
+}
+
 fn gen_big_random(len: usize) -> Vec<[u64; 16]> {
     let mut rng = thread_rng();
     rng.gen_iter().map(|x| [x; 16]).take(len).collect()
@@ -82,28 +114,67 @@ macro_rules! sort_bench {
     }
 }
 
-sort_bench!(small_random_3bcda48a3, gen_random, 10);
-sort_bench!(small_ascending_3bcda48a3, gen_ascending, 10);
-sort_bench!(small_descending_3bcda48a3, gen_descending, 10);
+sort_bench!(small_random_3bcda48a3_s, gen_random, 10);
+sort_bench!(small_ascending_3bcda48a3_s, gen_ascending, 10);
+sort_bench!(small_descending_3bcda48a3_s, gen_descending, 10);
 
-sort_bench!(small_big_random_3bcda48a3, gen_big_random, 10);
-sort_bench!(small_big_ascending_3bcda48a3, gen_big_ascending, 10);
-sort_bench!(small_big_descending_3bcda48a3, gen_big_descending, 10);
+sort_bench!(small_big_random_3bcda48a3_s, gen_big_random, 10);
+sort_bench!(small_big_ascending_3bcda48a3_s, gen_big_ascending, 10);
+sort_bench!(small_big_descending_3bcda48a3_s, gen_big_descending, 10);
 
-sort_bench!(medium_random_3bcda48a3, gen_random, 100);
-sort_bench!(medium_ascending_3bcda48a3, gen_ascending, 100);
-sort_bench!(medium_descending_3bcda48a3, gen_descending, 100);
+sort_bench!(medium_random_3bcda48a3_s, gen_random, 100);
+sort_bench!(medium_ascending_3bcda48a3_s, gen_ascending, 100);
+sort_bench!(medium_descending_3bcda48a3_s, gen_descending, 100);
 
-sort_bench!(large_short_runs_3bcda48a3, gen_short_runs, 10000);
-sort_bench!(large_random_3bcda48a3, gen_random, 10000);
-sort_bench!(large_ascending_3bcda48a3, gen_ascending, 10000);
-sort_bench!(large_descending_3bcda48a3, gen_descending, 10000);
-sort_bench!(large_mostly_ascending_3bcda48a3, gen_mostly_ascending, 10000);
-sort_bench!(large_mostly_descending_3bcda48a3, gen_mostly_descending, 10000);
+sort_bench!(large_short_runs_3bcda48a3_s, gen_short_runs, 10000);
+sort_bench!(large_random_3bcda48a3_s, gen_random, 10000);
+sort_bench!(large_ascending_3bcda48a3_s, gen_ascending, 10000);
+sort_bench!(large_descending_3bcda48a3_s, gen_descending, 10000);
+sort_bench!(large_mostly_ascending_3bcda48a3_s, gen_mostly_ascending, 10000);
+sort_bench!(large_mostly_descending_3bcda48a3_s, gen_mostly_descending, 10000);
 
-sort_bench!(large_big_random_3bcda48a3, gen_big_random, 10000);
-sort_bench!(large_big_ascending_3bcda48a3, gen_big_ascending, 10000);
-sort_bench!(large_big_descending_3bcda48a3, gen_big_descending, 10000);
+sort_bench!(nightmare_3bcda48a3_s, gen_nightmare, 1);
+sort_bench!(marenight_3bcda48a3_s, gen_marenight, 1);
+
+sort_bench!(large_big_random_3bcda48a3_s, gen_big_random, 10000);
+sort_bench!(large_big_ascending_3bcda48a3_s, gen_big_ascending, 10000);
+sort_bench!(large_big_descending_3bcda48a3_s, gen_big_descending, 10000);
+
+macro_rules! unstable_sort_bench {
+    ($name:ident, $gen:expr, $len:expr) => {
+        #[bench]
+        fn $name(b: &mut Bencher) {
+            b.iter(|| $gen($len).sort_unstable());
+            b.bytes = $len * mem::size_of_val(&$gen(1)[0]) as u64;
+        }
+    }
+}
+
+unstable_sort_bench!(small_random_3bcda48a3_u, gen_random, 10);
+unstable_sort_bench!(small_ascending_3bcda48a3_u, gen_ascending, 10);
+unstable_sort_bench!(small_descending_3bcda48a3_u, gen_descending, 10);
+
+unstable_sort_bench!(small_big_random_3bcda48a3_u, gen_big_random, 10);
+unstable_sort_bench!(small_big_ascending_3bcda48a3_u, gen_big_ascending, 10);
+unstable_sort_bench!(small_big_descending_3bcda48a3_u, gen_big_descending, 10);
+
+unstable_sort_bench!(medium_random_3bcda48a3_u, gen_random, 100);
+unstable_sort_bench!(medium_ascending_3bcda48a3_u, gen_ascending, 100);
+unstable_sort_bench!(medium_descending_3bcda48a3_u, gen_descending, 100);
+
+unstable_sort_bench!(large_short_runs_3bcda48a3_u, gen_short_runs, 10000);
+unstable_sort_bench!(large_random_3bcda48a3_u, gen_random, 10000);
+unstable_sort_bench!(large_ascending_3bcda48a3_u, gen_ascending, 10000);
+unstable_sort_bench!(large_descending_3bcda48a3_u, gen_descending, 10000);
+unstable_sort_bench!(large_mostly_ascending_3bcda48a3_u, gen_mostly_ascending, 10000);
+unstable_sort_bench!(large_mostly_descending_3bcda48a3_u, gen_mostly_descending, 10000);
+
+unstable_sort_bench!(nightmare_3bcda48a3_u, gen_nightmare, 1);
+unstable_sort_bench!(marenight_3bcda48a3_u, gen_marenight, 1);
+
+unstable_sort_bench!(large_big_random_3bcda48a3_u, gen_big_random, 10000);
+unstable_sort_bench!(large_big_ascending_3bcda48a3_u, gen_big_ascending, 10000);
+unstable_sort_bench!(large_big_descending_3bcda48a3_u, gen_big_descending, 10000);
 
 macro_rules! new_sort_bench {
     ($name:ident, $gen:expr, $len:expr) => {
@@ -133,6 +204,9 @@ new_sort_bench!(large_ascending_gidsort, gen_ascending, 10000);
 new_sort_bench!(large_descending_gidsort, gen_descending, 10000);
 new_sort_bench!(large_mostly_ascending_gidsort, gen_mostly_ascending, 10000);
 new_sort_bench!(large_mostly_descending_gidsort, gen_mostly_descending, 10000);
+
+new_sort_bench!(nightmare_gidsort, gen_nightmare, 1);
+new_sort_bench!(marenight_gidsort, gen_marenight, 1);
 
 new_sort_bench!(large_big_random_gidsort, gen_big_random, 10000);
 new_sort_bench!(large_big_ascending_gidsort, gen_big_ascending, 10000);
